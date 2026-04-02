f3b49f95644dc9f8c99de75dbb21603ec3ec1948
Steven Wilkin <stevenwilkin@gmail.com>
2026-04-02 13:22:34 +0100

    Don't show debug mode banner

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MainScreen(),
     );
   }
 }
-
